c7e1ec60a8bf203006f18298fc1b2dd4c2e5436f
mike <michael.noman70@bcmail.cuny.edu>
2021-04-22 12:34:07 -0400

    Added bookmark route

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Landing from "./_components/Home/Landing.js";
 import Login from "./_components/User/Login.js";
 import Signup from "./_components/User/Signup.js";
 import Profile from "./_components/Profile/Profile.js";
+import Bookmarks from "./_components/Bookmarks/Bookmarks.js";
 import "./App.css";
 import "semantic-ui-css/semantic.min.css";
 
@@ -24,6 +25,10 @@ function App() {
 
       <Switch>
         <Route path="/profile" component={Profile} />
+      </Switch>
+
+      <Switch>
+        <Route path="/bookmarks" component={Bookmarks} />
       </Switch>
     </div>
   );
