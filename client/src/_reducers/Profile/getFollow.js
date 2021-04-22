db9637be6c3cf1b2cb362637b260075d912daabf
mike <michael.noman70@bcmail.cuny.edu>
2021-04-22 12:33:40 -0400

    Removed a log

--- client/src/_reducers/Profile/getFollow.js
+++ client/src/_reducers/Profile/getFollow.js
@@ -1,11 +1,9 @@
 const getFollow = (initData = { Followers: "0", Following: "0" }, action) => {
   switch (action.type) {
     case "GET_FOLLOWER":
-      console.log(action.payload.data.followerCount);
       localStorage.setItem("followers", action.payload.data.followerCount);
       return { ...initData, Followers: action.payload.data.followerCount };
     case "GET_FOLLOWING":
-      console.log(action.payload.data.followingCount);
       localStorage.setItem("following", action.payload.data.followingCount);
       return { ...initData, Following: action.payload.data.followingCount };
     default:
