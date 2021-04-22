721005dbe311b63ffaa21ad4df9f37068cf488c2
mike <michael.noman70@bcmail.cuny.edu>
2021-04-22 12:34:39 -0400

    Added bookmark reducers

--- client/src/_reducers/combine.js
+++ client/src/_reducers/combine.js
@@ -3,12 +3,14 @@ import login from "./User/login";
 import signup from "./User/signup";
 import getPosts from "./Profile/handlePosts";
 import getFollow from "./Profile/getFollow";
+import bookmarks from "./Bookmarks/bookmarks";
 
 const combined = combineReducers({
   login,
   signup,
   getPosts,
   getFollow,
+  bookmarks,
 });
 
 export default combined;
